b8706aff6fa7ee13d59c78a5338ae5aa8d719e15
Brett Lombard <brett@thymetech.co.za>
2019-10-11 13:33:35 +0200

    Injector working

---
+++ src/Loader/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using Bleak;
+
+namespace Loader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessStartInfo processStartInfo = new ProcessStartInfo(@"D:\Coding\MemoryLearning\test-exe\wow.exe");
+            Process process = Process.Start(processStartInfo);
+            Thread.Sleep(100);
+
+            using (var injector = new Injector("Wow", @"D:\Coding\MemoryLearning\src\x64\Debug\InjectMeDLL.dll", InjectionMethod.CreateThread, InjectionFlags.HideDllFromPeb | InjectionFlags.RandomiseDllHeaders | InjectionFlags.RandomiseDllName))
+            {
+                // Inject the DLL into the remote process
+                var dllBaseAddress = injector.InjectDll();
+                Console.WriteLine($"DLL Injected its base address is: 0x{dllBaseAddress:X}");
+
+                Console.WriteLine("Press <Enter> key to eject DLL...");
+                // Eject the DLL from the process
+                Console.ReadLine();
+
+                injector.EjectDll();
+                Console.WriteLine("Dll Ejected.");
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
